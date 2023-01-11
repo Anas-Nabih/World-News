3f32be5c0979ada9b7efd8e2333d31764a7d0d3d
Your NameAnas-Nabih <anas7nabih@gmail.com>
2023-01-11 17:25:47 +0200

    handle news item container

--- lib/widgets/news_item.dart
+++ lib/widgets/news_item.dart
@@ -1,6 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:sizer/sizer.dart';
 import 'package:world_news/models/article_model.dart';
+import 'package:world_news/res/colors.dart';
 
 class NewsItem extends StatelessWidget {
   const NewsItem({
@@ -10,38 +13,94 @@ class NewsItem extends StatelessWidget {
 
   final Article article;
 
+  String dateFormatter(String date) {
+    final dateFormate = DateFormat("yyyy-MM-dd").format(DateTime.parse(date));
+    return dateFormate;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 25.h,
-      margin: EdgeInsets.only(bottom: 2.h,right: 2.w),
-      decoration: BoxDecoration(color: Colors.white,
+      margin: EdgeInsets.only(bottom: 2.h, right: 2.w),
+      decoration: BoxDecoration(
+          color: Colors.white,
           borderRadius: BorderRadius.circular(12),
-        boxShadow: [
-          BoxShadow(color: Colors.grey.withOpacity(0.2),
-          offset: Offset(3,2),
-            spreadRadius: 1,
-            blurRadius: 2
-
-          ),
-        ]
-      ),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                offset: const Offset(3, 2),
+                spreadRadius: 1,
+                blurRadius: 2),
+          ]),
       child: Row(
         children: [
           Container(
             height: 25.h,
             width: 35.w,
             decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(12),
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(12),
             ),
-          ),
-          SizedBox(
-            width: 50.w,
-            child: Text(article.title!,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 1,
+            child:CachedNetworkImage(
+              progressIndicatorBuilder: (context, url, progress) => Center(
+                child: CircularProgressIndicator(
+                  value: progress.progress,
+                ),
               ),
+              imageUrl:article.imageUrl!
+            ) ,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 1.h, horizontal: 2.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 50.w,
+                  child: Text(
+                    article.title!,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
+                ),
+                SizedBox(height: 2.h),
+                SizedBox(
+                  width: 50.w,
+                  child: Text(
+                    article?.content ?? article?.description ?? "",
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 5,
+                    style: TextStyle(color: Colors.black.withOpacity(.6)),
+                  ),
+                ),
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 18.w,
+                      child: Text(
+                        dateFormatter(article.publishedAt!),
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(color: MColors.kPrimaryColor),
+                      ),
+                    ),
+                    SizedBox(width: 24.w),
+                    SizedBox(
+                      width: 10.w,
+                      child: Text(
+                        article.source!.name!,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                            color: MColors.kPrimaryColor,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           )
         ],
       ),
